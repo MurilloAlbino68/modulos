33100920081499df502cb11cf86ca4fe0c72029a
jaini <jainisjustino@gmail.com>
2023-08-29 11:53:58 -0300

    add modules

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,13 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var divisao = require("./modulos/divisao");
+var multiplicacao = require("./modulos/multiplicacao");
 
+console.log('a soma dos numeros 20,90');
 console.log(somar(20,90));
+console.log('a media dos numeros 20,90');
 console.log(media(20,90));
+console.log('a divisão dos numeros 20,90');
+console.log(divisao(20,90));
+console.log('a multiplicação dos numeros 20,90');
+console.log(multiplicacao(20,90));
