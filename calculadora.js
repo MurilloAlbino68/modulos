787e19ecb0bb2a49432f4055c4e6f496cd2d9baa
jaini <jainisjustino@gmail.com>
2023-09-05 11:40:49 -0300

    sim

--- calculadora.js
+++ calculadora.js
@@ -2,6 +2,8 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var divisao = require("./modulos/divisao");
 var multiplicacao = require("./modulos/multiplicacao");
+var nome = "murillo";
+var msg = "Seja bem-vindo";
 
 console.log('a soma dos numeros 20,90');
 console.log(somar(20,90));
@@ -11,3 +13,4 @@ console.log('a divisão dos numeros 20,90');
 console.log(divisao(20,90));
 console.log('a multiplicação dos numeros 20,90');
 console.log(multiplicacao(20,90));
+console.log(mensagem(nome.msg));
